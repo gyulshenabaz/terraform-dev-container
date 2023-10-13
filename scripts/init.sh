df2b07c53a4c26bda67bfaff7e8f1a0b77e47a10
Gyulshen Abaz <gulshen.abaz@scalefocus.com>
2023-10-13 19:34:14 +0300

    Add .vscode

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,4 +6,5 @@ set -o pipefail
 
 git clone --branch=main --depth=1 git@github.com:gyulshenabaz/terraform-dev-container.git
 mv terraform-dev-container/.devcontainer .devcontainer
+mv terraform-dev-container/.vscode .vscode
 rm -rf terraform-dev-container
